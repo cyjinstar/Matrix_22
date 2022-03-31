f53111511f12ae65251523426a53d79979963dcd
YoungJin <cyjinstar@naver.com>
2022-03-31 17:59:14 +0900

    floatver2

--- reverse.c
+++ reverse.c
@@ -3,18 +3,18 @@
 #define _CRT_SECURE_NO_WARNINGS
 
 float** nbynImatrix();
-int** enterMatrix();
+float** enterMatrix();
 void reverseMat();
 
 int n = 0;
 float **Imatarr;
-int **inputMatarr;
+float **inputMatarr;
 
 int main(){
     Imatarr = nbynImatrix();
-    printf("%f, %f, %f, %f \n",Imatarr[0][0], Imatarr[0][1], Imatarr[1][0], Imatarr[1][1]); //testing
+    printf("%.0f, %.0f, %.0f, %.0f \n",Imatarr[0][0], Imatarr[0][1], Imatarr[1][0], Imatarr[1][1]); //testing
     inputMatarr = enterMatrix();
-    printf("%d, %d, %d, %d \n",inputMatarr[0][0], inputMatarr[0][1], inputMatarr[1][0], inputMatarr[1][1]); //testing
+    printf("%.0f, %.0f, %.0f, %.0f \n",inputMatarr[0][0], inputMatarr[0][1], inputMatarr[1][0], inputMatarr[1][1]); //testing
     reverseMat(inputMatarr,Imatarr,n);
     //free arrs_should do because of err.
     free(Imatarr);
@@ -35,30 +35,30 @@ float** nbynImatrix(){ //Function that makes I_matrix
         Imatrix[i] = (float*)malloc(sizeof(float) * n);
         for (int j = 0; j < n; j++) {
             if(i==j){
-                Imatrix[i][j] = 1;
+                Imatrix[i][j] = 1.0;
             }
             else{
-                Imatrix[i][j] = 0;
+                Imatrix[i][j] = 0.0;
             }
         }
     }
     for (int i = 0; i < n; i++) { //testing
         for (int j = 0; j < n; j++) {
-            printf("arr[%f][%f] : %f   ", i+1, j+1, Imatrix[i][j]);
+            printf("arr[%d][%d] : %.1f   ", i+1, j+1, Imatrix[i][j]);
         }
         printf("\n");
     }
     return Imatrix;
 }
 
-int** enterMatrix(){ //Input n*n Matrix & Investigate that det == 0 or not
+float** enterMatrix(){ //Input n*n Matrix & Investigate that det == 0 or not
     int num = 0;
-    int det = 0;
+    float det = 0;
     printf("enter %d by %d Matrix\n",n, n);
 
-    int** Inputmatrix = (int**)malloc(sizeof(int*) * n);
+    float** Inputmatrix = (float**)malloc(sizeof(float*) * n);
     for (int i = 0; i < n; i++) { 
-        Inputmatrix[i] = (int*)malloc(sizeof(int) * n);
+        Inputmatrix[i] = (float*)malloc(sizeof(float) * n);
         for (int j = 0; j < n; j++) {
             printf("enter [%d][%d] ", i + 1, j + 1);
             scanf("%d",&num);
@@ -79,35 +79,46 @@ int** enterMatrix(){ //Input n*n Matrix & Investigate that det == 0 or not
         printf("Can't be reversed!!");
     }
     
-    printf("det = %d\n",det);//testing
+    printf("det = %.1f\n",det);//testing
     for (int i = 0; i < n; i++) { //testing
         for (int j = 0; j < n; j++) {
-            printf("arr[%d][%d] : %d   ", i+1, j+1, Inputmatrix[i][j]);
+            printf("arr[%d][%d] : %.1f ", i+1, j+1, Inputmatrix[i][j]);
         }
         printf("\n");
     }
     return Inputmatrix;
 }
 
-void reverseMat(int** Inputmatrix, float** Imatrix, int n){ //아직 안돌아감, 계산이 안됨
-    int row = 0;
-    int col = 0;
+void divi(float** Inputmatrix,float** Imatrix){
+    for(int a = 0; a < n; a++){
 
-    if(Inputmatrix[0][0] != 1){
-        for(int i = 0; i < n; i++){  
-        Inputmatrix[0][i] = Inputmatrix[0][i] / Inputmatrix[0][0];
-        Imatrix[0][i] = Imatrix[0][i] / Inputmatrix[0][0];
+        for(int b = 0; b < n; b++){
+
+            if(Inputmatrix[a][b] != 1){
+                float k = Inputmatrix[0][0];
+
+                for(int i = 0; i < n; i++){
+                    Inputmatrix[a][i] = Inputmatrix[a][i] / k;
+                    Imatrix[a][i] = Imatrix[a][i] / k;
+                    }
+            }
         }
     }
+}
+
+void reverseMat(float** Inputmatrix, float** Imatrix, int n){ //아직 안돌아감, 계산이 안됨
+
+    divi(Inputmatrix,Imatrix);
 
     for(int i = 1; i < n; i++){
 
         if(Inputmatrix[i][0] != 0){
-        int k = Inputmatrix[i][0];
+        float k = Inputmatrix[i][0];
 
             for(int j = 0; j < n; j++){
                 Inputmatrix[i][j] -= Inputmatrix[i-1][j] * k;
                 Imatrix[i][j] -= Imatrix[i-1][j] * k;
+                divi(Inputmatrix,Imatrix);
             }
 
         }
@@ -115,13 +126,13 @@ void reverseMat(int** Inputmatrix, float** Imatrix, int n){ //아직 안돌아�
 
     for (int i = 0; i < n; i++) { //testing
         for (int j = 0; j < n; j++) {
-            printf("arr[%d][%d] : %d   ", i+1, j+1, Inputmatrix[i][j]);
+            printf("arr[%d][%d] : %.3f   ", i+1, j+1, Inputmatrix[i][j]);
         }
         printf("\n");
     }
     for (int i = 0; i < n; i++) { //testing
         for (int j = 0; j < n; j++) {
-            printf("arr[%d][%d] : %d   ", i+1, j+1, Imatrix[i][j]);
+            printf("arr[%d][%d] : %.3f   ", i+1, j+1, Imatrix[i][j]);
         }
         printf("\n");
     }
